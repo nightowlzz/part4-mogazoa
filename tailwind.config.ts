c7fa0ca96fb3af9cc64e0ce6cb0a3bf02b2799b4
hello <yup299@gmail.com>
2024-07-04 16:12:18 +0900

    chore : 초기 프로젝트 구조 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: {
+          '400': '#2E2E3A',
+          '500': '#21212A',
+          '600': '#17171C',
+        },
+        white: '#F1F1F5',
+        gray: {
+          '500': '#9FA6B2',
+          '600': '#6E6E82'
+        },
+        blue: '#5097FA',
+        indigo: '#5363FF',
+        yellow: '#FFC83C',
+        green: '#05D58B',
+        pink: '#FF2F9F',
+        red: '#FF0000'
+      },
+      fontSize: {
+        sm: '14px',
+        base: '16px',
+      },
+      screens: {
+        m: '375px',
+        t: '744px',
+        p: '1280px',
       },
     },
   },
