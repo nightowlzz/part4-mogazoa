7de823428f17da519e8819806a1690b34d6e36f8
hello <yup299@gmail.com>
2024-07-05 16:31:24 +0900

    fix : axios instance를 수정하였습니다.

--- utils/instance.ts
+++ utils/instance.ts
@@ -1,6 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 5000,
+const BASE_URL = 'https://mogazoa-api.vercel.app';
+const TEAM_ID = '5-6';
+const token = localStorage.getItem('Token');
+
+const instance: AxiosInstance = axios.create({
+  baseURL: `${BASE_URL}/${TEAM_ID}/`,
+  headers: {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  },
 });
+
+export default instance;
