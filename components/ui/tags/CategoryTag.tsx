8799aca0809b3cfaf968077baeede0daa5112df1
foxholic9 <roy0915z@naver.com>
2024-07-09 18:12:09 +0900

    Feat: 폴더명 변경, 비교 태그 기능 구현, 색상 수정, 유틸 함수 추가
    stringToFraction : 문자열을 소수화합니다. Math.random()과 다르게 같은 문자열이면 같은 소수가 나옵니다.
    
    반응형: 태블릿(768px) 이상/이하로 변경했습니다.
    
    색상 전부 테스트해보니 비슷해보이거나 안이쁜것들을 제거했습니다. 비슷한것들은 figma에 있는 것들 우선으로 남겼습니다. 현재 태그는 0부터 15까지 16개 색상이 있습니다.

--- components/ui/tags/CategoryTag.tsx
+++ components/ui/tags/CategoryTag.tsx
@@ -3,10 +3,11 @@ interface TagProps {
   categoryId: number;
 }
 
+// 카테고리 이름을 고유한 색으로 보여주는 태그 컴포넌트입니다.
 // 카테고리 id는 색상을 고유하게 만들기 위해서 사용합니다.
-function CategoryTag({ categoryName, categoryId = 2 }: TagProps) {
+function CategoryTag({ categoryName, categoryId = 1 }: TagProps) {
   // categoryId 값이 유효한 범위를 넘을 때 방어코드
-  const safeCategoryId = categoryId >= 0 && categoryId <= 30 ? categoryId : categoryId % 30;
+  const safeCategoryId = categoryId >= 0 && categoryId < 16 ? categoryId : categoryId % 16;
 
   // Tailwind CSS 클래스 이름 생성
   const textColorClass = `text-tagColor-${safeCategoryId}`;
@@ -14,7 +15,7 @@ function CategoryTag({ categoryName, categoryId = 2 }: TagProps) {
 
   return (
     <div
-      className={`flex items-center justify-self-center lg:px-[0.625rem] px-2 py-1 lg:h-[1.813rem] h-[1.375rem] w-fit rounded-lg lg:rounded-md text-xs lg:text-lg ${textColorClass} ${bgColorClass}`}
+      className={`flex items-center justify-self-center md:px-[0.625rem] px-2 py-1 md:h-[1.813rem] h-[1.375rem] w-fit rounded-lg text-xs md:text-lg ${textColorClass} ${bgColorClass}`}
     >
       {categoryName}
     </div>
--- components/ui/tags/RankingTag.tsx
+++ components/ui/tags/RankingTag.tsx
@@ -2,6 +2,7 @@ interface TagProps {
   rank: number;
 }
 
+// 랭킹을 표시하는 작은 태그입니다. 1, 2등은 고유한 색상을 사용합니다.
 function RankingTag({ rank = 3 }: TagProps) {
   // Tailwind CSS 클래스 이름 생성, 3등 이상은 gray-500으로 동일
   const textColorClass = rank === 1 ? `text-pink` : rank === 2 ? 'text-green' : 'text-gray-500';
@@ -9,7 +10,7 @@ function RankingTag({ rank = 3 }: TagProps) {
 
   return (
     <div
-      className={`flex items-center justify-self-center lg:px-2 px-[0.375rem] py-[0.5rem] lg:h-4 h-[1.125rem] w-fit rounded-full text-[0.625rem] lg:text-xs ${textColorClass} ${bgColorClass}`}
+      className={`flex items-center justify-self-center md:px-2 px-[0.375rem] py-[0.5rem] md:h-4 h-[1.125rem] w-fit rounded-full text-[0.625rem] md:text-xs ${textColorClass} ${bgColorClass}`}
     >
       {rank}등
     </div>
