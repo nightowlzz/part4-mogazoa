b33a86fc6bb8fcaaa477fce780aee4a02d8e4ba4
foxholic9 <roy0915z@naver.com>
2024-07-10 17:43:19 +0900

    Feat: Thumbs, 카테고리필터 디자인 추가
    간단하게 좋아요 숫자를 +-하는 기능을 추가한 버튼입니다.
    CategoryFilter : 모바일 화면에서 나타나는 컴포넌트로, 현재 선택 중인 카테고리가 표시됩니다.
     기본값일 경우 '카테고리'를 표시합니다. 폰트 색상이 조금 다릅니다.

---
+++ app/_styled-guide/_components/CategoryFilter.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { TbTriangleSquareCircle } from 'react-icons/tb';
+
+interface CategoryFilterProps {
+  category?: string;
+}
+
+// 모바일 화면에서 나타나는 컴포넌트로, 현재 선택 중인 카테고리가 표시됩니다.
+// 기본값일 경우 '카테고리'를 표시합니다. 폰트 색상이 조금 다릅니다.
+const CategoryFilter = (props: CategoryFilterProps) => {
+  const { category } = props;
+
+  return (
+    <div className="flex items-center align-middle px-[10px] md:px-3 py-[6px] w-fit bg-black-450 border-gray-650 border rounded-full">
+      <TbTriangleSquareCircle className="text-gray-500 mr-[5px] h-[18px] w-[18px]" />
+      {category ? (
+        <span className="text-gray-500 text-sm md:text-lg">{category}</span>
+      ) : (
+        <span className="text-gray-600 text-sm md:text-lg">카테고리</span>
+      )}
+    </div>
+  );
+};
+
+export default CategoryFilter;
