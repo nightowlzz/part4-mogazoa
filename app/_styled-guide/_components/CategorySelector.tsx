785321d813cf3246f50fe8c2d1584fae2da5e383
foxholic9 <roy0915z@naver.com>
2024-07-10 16:19:32 +0900

    Feat: 드롭다운 메뉴 컴포넌트 추가
    카테고리 선택 드롭다운과 정렬 기준 선택 드롭다운을 두 컴포넌트로 분리해서 개발했습니다. 구조는 거의 동일합니다.
    DropdownList는 드롭다운 메뉴를 클릭했을 때 나타나는 창 컴포넌트로, 검색창 추천검색기능에서도 재사용 하기 위해서 분리했습니다.
    
    선택지는 기본값으로 Figma 디자인에 있는 내용들을 넣어놨고, prop으로 받아 변경할 수도 있습니다.

---
+++ app/_styled-guide/_components/CategorySelector.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useState } from 'react';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import DropdownList from './DropdownList';
+
+interface CategorySelectorProps {
+  category?: string[];
+  placeHolder?: string;
+}
+
+// 카테고리 선택 기능을 위한 드롭다운 메뉴 컴포넌트입니다.
+// 기본값으로 10개의 카테고리를 선택할 수 있습니다.
+const CategorySelector = (props: CategorySelectorProps) => {
+  const options = [
+    '음악',
+    '영화/드라마',
+    '강의/책',
+    '호텔',
+    '가구/인테리어',
+    '식당',
+    '전자기기',
+    '화장품',
+    '의류/악세서리',
+    '앱',
+  ];
+  const { category = options, placeHolder = '카테고리 선택' } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(placeHolder);
+
+  // 버튼 이벤트
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative inline-block text-left w-full">
+      <div>
+        <button
+          type="button"
+          className="inline-flex justify-between align-middle w-full rounded-lg border-gray-700 hover:border-blue bg-black-450 px-5 py-[1.063rem] lg:py-[1.438rem] text-sm font-medium text-gray-600 hover:text-white"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={toggleDropdown}
+        >
+          {selectedItem}
+          <IoMdArrowDropdown
+            className={`ml-2 h-5 mr-[-4px] transition-transform duration-300 ${isOpen ? 'rotate-180' : ''}`}
+          />
+        </button>
+      </div>
+
+      {isOpen && (
+        <DropdownList options={category} onSelect={setSelectedItem} onClose={toggleDropdown} />
+      )}
+    </div>
+  );
+};
+
+export default CategorySelector;
