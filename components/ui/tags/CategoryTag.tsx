79e241af9343872afefccff930aefbb02b48fbcc
khs0727 <160032149+khs0727@users.noreply.github.com>
2024-07-11 22:02:34 +0900

    Update components/ui/tags/CategoryTag.tsx
    Co-authored-by: Nashil <choinashil@gmail.com>

--- components/ui/tags/CategoryTag.tsx
+++ components/ui/tags/CategoryTag.tsx
@@ -6,7 +6,7 @@ interface TagProps {
 
 // 카테고리 이름을 고유한 색으로 보여주는 태그 컴포넌트입니다.
 // 카테고리 id는 색상을 고유하게 만들기 위해서 사용합니다.
-function CategoryTag({ categoryName, categoryId = 1, className = '' }: TagProps) {
+function CategoryTag({ categoryName, categoryId = 1, className }: TagProps) {
   // categoryId 값이 유효한 범위를 넘을 때 방어코드
   const safeCategoryId = categoryId >= 0 && categoryId < 16 ? categoryId : categoryId % 16;
 
