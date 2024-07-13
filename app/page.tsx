41c1efc7b28b4516521bf7d5b22e4327bbab39cc
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-07-13 14:42:57 +0900

    feat: 상품 디테일 화면 생성 및 api 연동

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import StyledGuide from './_styled-guide/page';
-import ProductCard from './_styled-guide/_components/product-card';
 
 export default function Home() {
   return (
---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import ProductCard from '@/app/_styled-guide/_components/ProductCard';
+import { StatisticFavorite } from '@/app/_styled-guide/_components/statistics-favorite';
+import { StatisticRating } from '@/app/_styled-guide/_components/statistics-rating';
+import { StatisticReview } from '@/app/_styled-guide/_components/statistics-review';
+import { useGetProductDetail } from '@/hooks/product';
+import { useGetUserInfo } from '@/hooks/user';
+import { useParams } from 'next/navigation';
+
+export default function ProductPage() {
+  const { productId } = useParams();
+
+  const { data: productDetail, isLoading, error } = useGetProductDetail(Number(productId));
+  //const { data: userInfo } = useGetUserInfo(currentUser.id);
+
+  const currentUserId = 23; //임시
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="w-full h-full bg-[#1C1C22] flex flex-col items-center">
+      <div className="text-white">gnb</div>
+      <div className="mt-[60px] mb-[80px]">
+        <ProductCard
+          name={productDetail?.name || ''}
+          description={productDetail?.description || ''}
+          image={productDetail?.image || ''}
+          writerId={productDetail?.writerId || 0}
+          currentUserId={currentUserId}
+          categoryName={productDetail?.category?.name || ''}
+          categoryId={productDetail?.category.id || 0}
+        />
+      </div>
+      <div>
+        <h3 className="text-[#F1F1F5] text-xl font-normal">상품 통계</h3>
+        <div className="flex flex-col md:flex-row mt-[30px] mb-[60px] gap-[15px] lg:gap-5">
+          <StatisticRating
+            rating={productDetail?.rating || 0}
+            categoryMetric={
+              productDetail?.categoryMetric || { reviewCount: 0, favoriteCount: 0, rating: 0 }
+            }
+          />
+          <StatisticFavorite
+            favoriteCount={productDetail?.favoriteCount || 0}
+            categoryMetric={
+              productDetail?.categoryMetric || { reviewCount: 0, favoriteCount: 0, rating: 0 }
+            }
+          />
+          <StatisticReview
+            reviewCount={productDetail?.reviewCount || 0}
+            categoryMetric={
+              productDetail?.categoryMetric || { reviewCount: 0, favoriteCount: 0, rating: 0 }
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
