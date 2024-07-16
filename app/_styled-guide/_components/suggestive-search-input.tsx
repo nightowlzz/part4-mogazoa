24dd25f83635f3d1ab000953a7387221cd79fef8
foxholic9 <roy0915z@naver.com>
2024-07-16 11:58:34 +0900

    Feat: suggestive-search-input 컴포넌트 추가
    입력한 상품명을 검색해 드롭다운에 검색결과를 보여주고, 선택할 수 있는 컴포넌트입니다.
    선택지 클릭 시 동작과 엔터키 동작을 분리했습니다. onSelect는 필수지만 onEnter는 추가하지 않아도 됩니다.
    
    전에 입력한 내용을 기억해 검색어가 비었을 때 표시하게 해도 좋을 것 같습니다.

---
+++ app/_styled-guide/_components/suggestive-search-input.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import React, { useState, useRef } from 'react';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import useSearchSuggestions from '@/hooks/useSearchSuggestions';
+import DropdownList from './DropdownList';
+
+interface SuggestiveSearchInputProps {
+  keyword: string;
+  setKeyword: (keyword: string) => void;
+  onSelect: (option: string) => void;
+  onEnter?: (keyword: string) => void;
+  placeholder?: string;
+  boxClassName?: string;
+  inputClassName?: string;
+}
+
+/**
+ * 검색어를 포함하는 상품명들을 드롭다운으로 보여주는 인풋창입니다. 입력값이 유효한 상품명인지 알 수 없기 때문에 onSubmit과 onSelect를 분리했습니다.
+ *
+ * @param {Object} props - SearchInputProps
+ * @param {string} props.keyword - 인풋의 값 (상위 컴포넌트의 state).
+ * @param {function} props.setKeyword - 인풋 변경 함수 (상위 컴포넌트의 setState).
+ * @param {function} props.onSelect - Dropdown에서 선택지를 골랐을 때의 동작.
+ * @param {function} [props.onEnter] - 인풋 창에서 내용을 입력한 뒤 엔터키를 눌렀을 때 동작.
+ * @param {string} [props.placeholder='상품 이름을 검색해 보세요'] - 인풋창이 비어있을 때 보여질 내용.
+ * @param {string} [props.boxClassName] - 인풋 박스의 Tailwind CSS 클래스 이름.
+ * @param {string} [props.inputClassName] - 인풋 내용의 Tailwind CSS 클래스 이름.
+ */
+const SuggestiveSearchInput = ({
+  keyword = '',
+  setKeyword,
+  onSelect,
+  onEnter,
+  placeholder = '상품 이름을 검색해 보세요',
+  boxClassName,
+  inputClassName,
+}: SuggestiveSearchInputProps) => {
+  const { suggestions, isLoading, isError } = useSearchSuggestions(keyword);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // 검색창 input 활성화시, dropdownList 열림
+  const handleFocus = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleBlur = () => {
+    // input 요소가 focus를 잃어도 0.2초동안 Dropdown을 유지합니다.
+    setTimeout(() => {
+      if (!inputRef.current?.contains(document.activeElement)) {
+        setIsDropdownOpen(false);
+      }
+    }, 200);
+  };
+
+  // 검색창에서 엔터 입력 시 동작. 검색어는 한 글자 이상 입력해야 함
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && keyword.trim() && onEnter) {
+      onEnter(keyword);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <div className={`flex justify-between items-center py-4 px-5 ${boxClassName}`}>
+        <input
+          ref={inputRef}
+          type="text"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          placeholder={placeholder}
+          className={`w-full bg-transparent focus:outline-none text-white ${inputClassName}`}
+        />
+        <button onClick={() => inputRef.current?.focus()}>
+          {!isDropdownOpen && <IoMdArrowDropdown />}
+        </button>
+      </div>
+
+      {isDropdownOpen && <DropdownList options={suggestions} onSelect={onSelect} />}
+    </div>
+  );
+};
+
+export default SuggestiveSearchInput;
