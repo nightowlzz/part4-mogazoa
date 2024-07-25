6d5954fa7e139d174de2f54dc44a30bdb29ed45e
kimmiso <vmffjrm@naver.com>
2024-07-25 16:49:20 +0900

    feat: refetchInterval 사용해서 1분 마다 주기적으로 데이터 업데이트

--- app/(public)/_components/ranking-list.tsx
+++ app/(public)/_components/ranking-list.tsx
@@ -12,12 +12,19 @@ import { FreeMode, Mousewheel, Scrollbar } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Ranking from './ranking';
 export default function RankingList() {
-  const { data: userRanking, isPending, isError } = useGetUserRanking();
+  const {
+    data: userRanking,
+    isPending,
+    isError,
+  } = useGetUserRanking({
+    refetchInterval: 1000 * 60,
+  });
+
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1279);
+      setIsMobile(window.innerWidth <= 1280);
     };
 
     window.addEventListener('resize', handleResize);
@@ -35,7 +42,10 @@ export default function RankingList() {
   return (
     <>
       <aside className={cn(styled['main-ranking'], 'pt-[45px] lx:px-[30px] overflow-hidden')}>
-        <h2 className="font-sm lg:font-base text-white pb-5">리뷰어 랭킹</h2>
+        <h2 className="font-sm lg:font-base text-white pb-5">
+          리뷰어 랭킹{' '}
+          <small className="inline-block xl:block text-gray-600">(실시간 랭킹: 1분 단위)</small>
+        </h2>
         {!isMobile ? (
           <ul className="xl:space-y-7 overflow-x-auto">
             {userRanking.slice(0, 5).map((user, i) => (
