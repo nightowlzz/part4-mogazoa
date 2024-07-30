e273f570e34d9fc0facc094312dbbb561677870f
kimmiso <vmffjrm@naver.com>
2024-07-30 17:26:18 +0900

    fix: 한글, 띄어쓰기 있는 이미지파일 이름 재설정하여 업로드, 이미지 업로드 확장자 변경

--- utils/textUtils.ts
+++ utils/textUtils.ts
@@ -9,11 +9,18 @@ function truncateText({ text, maxLength = MAX_TEXTAREA_LENGTH }: TextWithMaxLeng
   return text.length > maxLength ? text.slice(0, maxLength) : text;
 }
 
-// textarea 글자
+// [NOTE] textarea 글자
 export function limitText(params: TextWithMaxLength): string {
   return truncateText(params);
 }
-// textarea 글자 수
+// [NOTE] textarea 글자 수
 export function limitTextLength(params: TextWithMaxLength): number {
   return truncateText(params).length;
 }
+
+// [NOTE] 이미지 파일명 변경
+export const renameFileWithExtension = (file: File) => {
+  const extension = file.name.split('.').pop();
+  const newFile = new File([file], `test.${extension}`, { type: file.type });
+  return newFile;
+};
