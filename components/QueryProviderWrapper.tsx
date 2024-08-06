1e7067f8f5974af0cade29d889e9c30c0ab2fbb2
hello <yup299@gmail.com>
2024-08-06 23:17:49 +0900

    feat : 전역으로 스테일 타임 추가 , 간단한 경고 수정

--- components/QueryProviderWrapper.tsx
+++ components/QueryProviderWrapper.tsx
@@ -3,7 +3,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 export default function QueryProviderWrapper({ children }: { children: React.ReactNode }) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
