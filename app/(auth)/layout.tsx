0fdceaa6860433f9b3b720850f7cc1596a33e879
kimmiso <vmffjrm@naver.com>
2024-08-07 17:03:24 +0900

    fix: 로그인, 회원가입 윈도우창 height 작을때 짤리는 현상 수정

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,10 +3,8 @@ import { cn } from '@/lib/utils';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section
-      className={cn(styled['content-full-height'], 'flex items-center justify-center h-screen')}
-    >
-      <div className="w-full max-w-[680px] pt-[30px] pb-[40px] px-5">{children}</div>
+    <section className={cn(styled['content-full-height'], 'flex items-center justify-center')}>
+      <div className="w-full max-w-[680px] px-5">{children}</div>
     </section>
   );
 }
