41d205183e261ebb42e3c43b08b88ea5e94fb459
foxholic9 <roy0915z@naver.com>
2024-08-07 12:08:23 +0900

    feat: 로딩 컴포넌트 이미지 및 메세지 수정
    현재 이 컴포넌트 사용하는 부분이 비교하기 페이지 뿐인것으로 확인되서 컴포넌트에서 수정했습니다.

--- app/_styled-guide/_components/loading.tsx
+++ app/_styled-guide/_components/loading.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="relative w-[79px] md:w-[87px] h-[73px] md:h-[84px]">
-      <Image src="/assets/images/loading.svg" alt="Loading" fill priority />
+    <div className="flex flex-col items-center gap-3 md:gap-5">
+      <div className="relative w-[50px] md:w-[70px] h-[40px] md:h-[50px]">
+        <Image src="/assets/images/loading.svg" alt="Loading" fill priority />
+      </div>
+      <div className="text-gray-500 text-base md:text-lg"> 비교할 항목을 모두 추가해 보세요</div>
     </div>
   );
 }
